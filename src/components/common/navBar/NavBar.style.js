3c1f2fe14c2e6448f761c384f361941588dd064d
Whatdoyumin <lym4963@naver.com>
2023-11-16 19:59:47 +0900

    💄 style: navBar 크기 스타일 변경

--- src/components/common/navBar/NavBar.style.js
+++ src/components/common/navBar/NavBar.style.js
@@ -1,41 +1,37 @@
-import { styled } from 'styled-components';
+import styled from 'styled-components';
 
 export const BottomNav = styled.nav`
+  max-width: var(--size-max-width);
+  width: 100%;
+  margin-left: calc(-1 * var(--size-gap));
   overflow: hidden;
   position: fixed;
   bottom: 0;
-  left: 0;
-  right: 0;
+  background-color: white;
   box-shadow: 0px -2px 8px rgba(0, 0, 0, 0.2);
 `;
 
 export const BottomDiv = styled.div`
   display: flex;
   justify-content: space-evenly;
-  float: left;
   width: 100%;
   height: 94px;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-  padding: 4px;
+  padding: 12px;
 `;
 
 export const NavDiv = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
   width: 48px;
   height: 52px;
-  padding: 8px 4px;
 `;
 
-export const NavIcon = styled.img`
-  width: 32px;
-  height: 32px;
-  padding: 0;
-`;
+export const NavIcon = styled.img``;
 
 export const NavText = styled.div`
-  font-size: 10px;
+  font-size: var(--font-size-caption);
   line-height: 20px;
   text-align: center;
 `;
