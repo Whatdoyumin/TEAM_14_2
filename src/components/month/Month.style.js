40d3a34ccb69d024858c59a5b22e7e844374c152
Whatdoyumin <lym4963@naver.com>
2023-11-17 21:05:18 +0900

    ✨ feat: Month 컴포넌트 추가

---
+++ src/components/month/Month.style.js
@@ -0,0 +1,16 @@
+import { styled } from 'styled-components';
+import GlobalFont from '../../styles/GlobalFont';
+
+export const CurrentMonth = styled.div`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: 18px;
+  text-align: center;
+  font-family: 'NPSfontBold', sans-serif;
+`;
+
+export const GlobalStyles = GlobalFont;
+
+export default CurrentMonth;
