bba3d0334196471d773a90d1fc768baf4c09bcc3
Whatdoyumin <lym4963@naver.com>
2023-11-17 10:32:15 +0900

    💄 style: appBar(상단바) 스타일 코드 형식 변경

--- src/components/common/appBar/AppBar.jsx
+++ src/components/common/appBar/AppBar.jsx
@@ -1,15 +1,15 @@
-import { AppBarContainer, AppInfo, PageTitle } from './AppBar.style';
+import * as S from './AppBar.style';
 import mypage from '../../../assets/images/mypage.svg';
 
 export default function AppBar({ currentPage, showInfoBtn, onHelpBtnClick }) {
   return (
-    <AppBarContainer>
-      <PageTitle>{currentPage}</PageTitle>
+    <S.AppBarContainer>
+      <S.PageTitle>{currentPage}</S.PageTitle>
       {showInfoBtn && (
-        <AppInfo onClick={onHelpBtnClick}>
+        <S.AppInfo onClick={onHelpBtnClick}>
           <img alt="" src={mypage} />
-        </AppInfo>
+        </S.AppInfo>
       )}
-    </AppBarContainer>
+    </S.AppBarContainer>
   );
 }
