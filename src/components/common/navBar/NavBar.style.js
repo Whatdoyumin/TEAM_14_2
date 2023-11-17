32e47a66576b320fddaf08423b0b83eadd266d70
jisooyoon <yoonmallang@gmail.com>
2023-11-18 02:17:41 +0900

    💄 style: 내브바 컴포넌트 스타일 수정

--- src/components/common/navBar/NavBar.style.js
+++ src/components/common/navBar/NavBar.style.js
@@ -8,7 +8,7 @@ export const BottomNav = styled.nav`
   position: fixed;
   bottom: 0;
   background-color: white;
-  box-shadow: 0px -2px 8px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px -2px 8px -2px rgba(0, 0, 0, 0.2);
 `;
 
 export const BottomDiv = styled.div`
@@ -26,6 +26,7 @@ export const NavDiv = styled.div`
   align-items: center;
   width: 48px;
   height: 52px;
+  cursor: pointer;
 `;
 
 export const NavIcon = styled.img``;
