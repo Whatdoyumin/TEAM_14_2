46afbfa4eac8c01fda842d6c4c4ae44fafcc3e94
Whatdoyumin <lym4963@naver.com>
2023-11-17 10:53:14 +0900

    💄 style: navBar(하단바) 스타일 코드 변경

--- src/components/common/navBar/NavBar.jsx
+++ src/components/common/navBar/NavBar.jsx
@@ -1,25 +1,25 @@
-import { BottomNav, BottomDiv, NavDiv, NavIcon, NavText } from './NavBar.style';
+import * as S from './NavBar.style';
 import home from '../../../assets/images/home.svg';
 import myReportEmpty from '../../../assets/images/report-empty.svg';
 import mypageEmpty from '../../../assets/images/mypage-empty.svg';
 
 export default function NavBar() {
   return (
-    <BottomNav>
-      <BottomDiv>
-        <NavDiv>
-          <NavIcon alt="" src={home} />
-          <NavText>홈</NavText>
-        </NavDiv>
-        <NavDiv>
-          <NavIcon alt="" src={myReportEmpty} />
-          <NavText>기록</NavText>
-        </NavDiv>
-        <NavDiv>
-          <NavIcon alt="" src={mypageEmpty} />
-          <NavText>설정</NavText>
-        </NavDiv>
-      </BottomDiv>
-    </BottomNav>
+    <S.BottomNav>
+      <S.BottomDiv>
+        <S.NavDiv>
+          <S.NavIcon alt="" src={home} />
+          <S.NavText>홈</S.NavText>
+        </S.NavDiv>
+        <S.NavDiv>
+          <S.NavIcon alt="" src={myReportEmpty} />
+          <S.NavText>기록</S.NavText>
+        </S.NavDiv>
+        <S.NavDiv>
+          <S.NavIcon alt="" src={mypageEmpty} />
+          <S.NavText>설정</S.NavText>
+        </S.NavDiv>
+      </S.BottomDiv>
+    </S.BottomNav>
   );
 }
