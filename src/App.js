458f6978c00f3b4051a776653c784e610dd05280
Whatdoyumin <lym4963@naver.com>
2023-11-17 13:48:06 +0900

    ✨ feat: 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+import Routes from './routes/Router';
 import GlobalFont from './styles/GlobalFont';
 import GlobalStyle from './styles/GlobalStyle';
-import NavBar from './components/common/navBar/NavBar';
 
 function App() {
   return (
     <>
       <GlobalFont />
       <GlobalStyle />
-      App
-      <NavBar />
+      <Routes />
     </>
   );
 }
