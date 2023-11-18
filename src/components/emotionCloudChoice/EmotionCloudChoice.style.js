3822e4fe78343d05248520a70eb4df0c92ac28b9
jisooyoon <yoonmallang@gmail.com>
2023-11-18 13:51:12 +0900

    💄 style: EmotionChoice 페이지 안 컴포넌트 스타일 수정

--- src/components/emotionCloudChoice/EmotionCloudChoice.style.js
+++ src/components/emotionCloudChoice/EmotionCloudChoice.style.js
@@ -1,54 +1,45 @@
 import { styled } from 'styled-components';
-import GlobalFont from '../../styles/GlobalFont';
 
 export const EmotionChoiceContainer = styled.div`
-  width: 100%;
-  height: 300px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 25px;
-`;
+  position: relative;
+  min-height: 320px;
+  text-align: center;
+  font-size: var(--font-size-subhead);
 
-export const EmotionMent = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  font-size: 16px;
-  h4 {
+  strong {
     font-family: 'NPSfontBold', sans-serif;
     margin: 0 0 0 4px;
   }
 `;
 
 export const EmotionGrid = styled.div`
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  transform: translateX(-50%);
+  width: 100%;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 20px;
-  margin-top: ${({ isClicked }) => (isClicked ? '10px' : '70px')};
-  button {
-    background-color: white;
+
+  input {
+    display: none;
+  }
+
+  img {
+    opacity: 0.5;
+    cursor: pointer;
+  }
+
+  img.select {
+    opacity: 1;
   }
 `;
 
 export const EmotionChoiceBtn = styled.div`
   max-width: var(--size-max-width);
   width: 100%;
-  bottom: 0;
-  position: fixed;
   display: flex;
   justify-content: space-between;
-  margin-bottom: 40px;
-
-  & > *:first-child {
-    margin-left: 15px;
-    margin-right: 10px;
-  }
-
-  & > *:nth-child(2) {
-    margin-right: 15px;
-  }
+  gap: 8px;
 `;
-
-export const GlobalStyles = GlobalFont;
