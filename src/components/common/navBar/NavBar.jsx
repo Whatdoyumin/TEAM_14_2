b0b8f44eab65c28710180f7d4489e846f6bdb339
Whatdoyumin <lym4963@naver.com>
2023-11-18 09:35:45 +0900

    ✨ feat: 네브바 페이지별 아이콘 변경

--- src/components/common/navBar/NavBar.jsx
+++ src/components/common/navBar/NavBar.jsx
@@ -1,25 +1,33 @@
 import { useNavigate } from 'react-router-dom';
 import * as S from './NavBar.style';
 import home from '../../../assets/images/home.svg';
+import homeEmpty from '../../../assets/images/home-empty.svg';
+import myReport from '../../../assets/images/report.svg';
 import myReportEmpty from '../../../assets/images/report-empty.svg';
+import mypage from '../../../assets/images/mypage.svg';
 import mypageEmpty from '../../../assets/images/mypage-empty.svg';
 
 export default function NavBar() {
   const navigate = useNavigate();
+  const path = window.location.pathname;
+
+  const getHomeImage = () => (path === '/main' ? home : homeEmpty);
+  const getReportImage = () => (path === '/report' ? myReport : myReportEmpty);
+  const getMypageImage = () => (path === '/mypage' ? mypage : mypageEmpty);
 
   return (
     <S.BottomNav>
       <S.BottomDiv>
         <S.NavDiv onClick={() => navigate('/main')}>
-          <S.NavIcon alt="" src={home} />
+          <S.NavIcon alt="" src={getHomeImage()} />
           <S.NavText>홈</S.NavText>
         </S.NavDiv>
         <S.NavDiv onClick={() => navigate('/report')}>
-          <S.NavIcon alt="" src={myReportEmpty} />
+          <S.NavIcon alt="" src={getReportImage()} />
           <S.NavText>기록</S.NavText>
         </S.NavDiv>
         <S.NavDiv onClick={() => navigate('/mypage')}>
-          <S.NavIcon alt="" src={mypageEmpty} />
+          <S.NavIcon alt="" src={getMypageImage()} />
           <S.NavText>설정</S.NavText>
         </S.NavDiv>
       </S.BottomDiv>
