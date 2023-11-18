e2936b0306e8630c0b2bbcfe09eec00f63aeb3b1
Whatdoyumin <lym4963@naver.com>
2023-11-18 22:39:34 +0900

    ✨ feat: EmotionSky 컴포넌트 EmotionTotalCount 멘트 추가

--- src/components/emotionSky/EmotionSky.jsx
+++ src/components/emotionSky/EmotionSky.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Month from '../month/Month';
 import MonthlyReport from '../monthlyReport/MonthlyReport';
-import EmotionSkyContainer from './EmotionSky.style';
+import { EmotionSkyContainer, MonthContainer, MonthCloudCount } from './EmotionSky.style';
 
 export default function EmotionSky() {
   return (
     <>
       <EmotionSkyContainer />
-      <Month />
       <MonthlyReport />
+      <MonthContainer>
+        <MonthCloudCount>
+          <span>총 </span>
+          <span>18</span>
+          <span>개의 구름을 띄웠어요!</span>
+        </MonthCloudCount>
+        <Month />
+      </MonthContainer>
     </>
   );
 }
