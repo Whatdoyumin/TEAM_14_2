e0879f30f4cfc891d138941eefdf4eec0221b212
jisooyoon <yoonmallang@gmail.com>
2023-11-18 14:00:49 +0900

    🌱 chore: 라우터 경로 수정

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,8 +6,9 @@ import Mypage from '../pages/mypage/Mypage';
 import Character from '../pages/character/Character';
 import Signin from '../pages/signin/Signin';
 import Signup from '../pages/signup/Signup';
-import DiaryTemplate from '../components/diaryTemplate/DiaryTemplate';
 import Splash from '../pages/splash/Splash';
+import DiaryEdit from '../pages/diary/edit/DiaryEdit';
+import DiaryView from '../pages/diary/view/DiaryView';
 
 export default function Router() {
   return (
@@ -20,10 +21,12 @@ export default function Router() {
         <Route path="/report" element={<Report />} />
         <Route path="/emotionChoice" element={<EmotionChoice />} />
         <Route path="/diary/*" element={<Outlet />}>
-          <Route path="write" element={<DiaryTemplate writeable />} />
+          <Route path="write" element={<DiaryEdit />} />
+
+          {/* DiaryView는 date, emotion, image, text 인자로 받아서 지난 일기 보여줄 수 있습니다 */}
           <Route
             path="view"
-            element={<DiaryTemplate date="2023-11-09" emotion="happy" text="일기텍스트" />}
+            element={<DiaryView date="2023-11-09" emotion="happy" text="일기텍스트" />}
           />
         </Route>
         <Route path="/mypage" element={<Mypage />} />
