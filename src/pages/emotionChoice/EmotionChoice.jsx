a78fd35118d78aeab02703193c7c50d6393774f8
Whatdoyumin <lym4963@naver.com>
2023-11-18 10:00:38 +0900

    ✨ feat: 감정구름 고르기 페이지 추가

--- src/pages/emotionChoice/EmotionChoice.jsx
+++ src/pages/emotionChoice/EmotionChoice.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import AppBar from '../../components/common/appBar/AppBar';
+import EmotionQuestion from '../../components/emotionQuestion/EmotionQuestion';
+import EmotionCloudChoice from '../../components/emotionCloudChoice/EmotionCloudChoice';
 
 export default function EmotionChoice() {
-  return <div>EmotionChoice</div>;
+  return (
+    <>
+      <AppBar currentPage="감정구름 고르기" />
+      <EmotionQuestion />
+      <EmotionCloudChoice />
+    </>
+  );
 }
